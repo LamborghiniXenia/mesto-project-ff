429f12f3499cd0d50b0363a982a5fdb00930d56a
Xenia <zhukovaxenia89@yandex>
2024-06-26 14:06:02 +0400

    first version

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,20 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
+const card = cardTemplate.querySelector('.card');
+function createCard(cardElement, removeCard) {
+    const placesItem = card.cloneNode(true);
+    const cardTitle = placesItem.querySelector('.card__title')
+    cardTitle.textContent = cardElement.name;
+    const cardImage = placesItem.querySelector('img')
+    cardImage.alt = cardElement.name;
+    cardImage.src = cardElement.link;
+    const deleteButton = placesItem.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', removeCard);
+    return placesItem;
+};
+function DeleteCard(e) {
+    e.target.closest('.card').remove()
+};
+initialCards.forEach(function (arr) {
+    placesList.append(createCard(arr, DeleteCard))
+})
